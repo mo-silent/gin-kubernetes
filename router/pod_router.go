2f9274d41808c8f6f3e8e0367c510fd943e63d56
mogd <1916393131@qq.com>
2022-06-10 20:14:02 +0800

    20220610: modify router naming

--- router/pod_router.go
+++ router/pod_router.go
@@ -12,10 +12,10 @@ func (p *PodRouter) InitPodRouter(Router *gin.RouterGroup) {
 	podGroup := Router.Group("pod")
 	podApi := v1.ApiGroupEnter.PodApi
 	{
-		podGroup.GET("getNamespacePod/:namespace", podApi.GetNamespacePod) // 获取命名空间的 pod 信息
-		podGroup.GET("getAllPod", podApi.GetAllPod)                        // 获取所有 pod 信息
-		podGroup.POST("getPod", podApi.GetPod)                             // 获取单个 pod 信息
-		podGroup.POST("createPod", podApi.CreatePod)                       // 创建 pod
-		podGroup.POST("deletePod", podApi.DeletePod)                       // 删除 pod
+		podGroup.GET("listNamespacePod/:namespace", podApi.ListNamespacePod) // 获取命名空间的所有 pod 信息
+		podGroup.GET("listAllPod", podApi.ListAllPod)                        // 获取所有 pod 信息
+		podGroup.POST("getPod", podApi.GetPod)                               // 获取单个 pod 信息
+		podGroup.POST("createPod", podApi.CreatePod)                         // 创建 pod
+		podGroup.POST("deletePod", podApi.DeletePod)                         // 删除 pod
 	}
 }
