62b0e90e96c8e8e49999ee1a0745063f18769a4e
mogd <1916393131@qq.com>
2022-06-24 11:14:11 +0800

    define unique index

--- model/system/user.go
+++ model/system/user.go
@@ -4,7 +4,7 @@ import "github.com/flipped-aurora/gin-vue-admin/server/global"
 
 type User struct {
 	global.GVA_MODEL
-	Username string `json:"userName" gorm:"comment:用户登录名"`
+	Username string `json:"username" gorm:"uniqueIndex:idx_username,length:255,comment:用户名"`
 	Password string `json:"password" gorm:"comment:登录密码"`
 	NickName string `json:"nickName" gorm:"default:系统用户;comment:用户昵称"`
 	Phone    string `json:"phone"  gorm:"comment:用户手机号"`
